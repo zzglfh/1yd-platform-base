69b20112363a395b4fcadbe705d8ccab999c9a36
zhuzhengguang <zhuzhengguang@tianjiancloud.com>
2015-03-19 16:20:46 +0800

    add config

--- src/main/resources/db/mysql/initDB.sql
+++ src/main/resources/db/mysql/initDB.sql
@@ -1,10 +1,10 @@
 
 
-CREATE TABLE IF NOT EXISTS `test` (
+CREATE TABLE IF NOT EXISTS test (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `creation_time` datetime DEFAULT NULL,
   `remove_mark` tinyint(4) DEFAULT NULL,
   `name` varchar(30) DEFAULT NULL,
   `test_type` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
