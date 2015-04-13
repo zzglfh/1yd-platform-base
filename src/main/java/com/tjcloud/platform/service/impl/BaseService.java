7f7967f70e7a760b21c2f07ce0f8078b61e2ad5f
zhuzhengguang <zhuzhengguang@tianjiancloud.com>
2015-04-14 00:18:17 +0800

    add use

--- src/main/java/com/tjcloud/platform/service/impl/BaseService.java
+++
@@ -1,8 +0,0 @@
-package com.tjcloud.platform.service.impl;
-
-import com.tjcloud.platform.component.BaseComponent;
-
-public abstract class BaseService extends BaseComponent {
-	
-
-}
--- src/main/java/com/tjcloud/platform/utils/DateUtil.java
+++
@@ -1,5 +0,0 @@
-package com.tjcloud.platform.utils;
-
-public class DateUtil {
-
-}
